90563a1265d1eba0e901b741b058c9e0c66fbd79
Nazar <wolfter12@gmail.com>
2021-12-29 00:30:53 +0200

    add dynamic image import with gray-matter

--- components/posts/post-detail/post-content/index.jsx
+++ components/posts/post-detail/post-content/index.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 
 import PostHeader from "../post-header";
@@ -11,10 +12,43 @@ function PostContent(props) {
 
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${slug}/${image.node.properties.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
     </article>
   );
 }
