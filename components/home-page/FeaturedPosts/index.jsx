9a813dd89934c147731d052316c812c773bd6a28
Nazar <wolfter12@gmail.com>
2021-12-28 22:08:04 +0200

    add posts

--- components/home-page/FeaturedPosts/index.jsx
+++ components/home-page/FeaturedPosts/index.jsx
@@ -1,8 +1,15 @@
-function FeaturedPosts() {
+import PostsGrid from "../../posts/PostsGrid";
+
+import classes from "./featured-posts.module.css";
+
+function FeaturedPosts(props) {
+  const { posts } = props;
+
   return (
-    <div>
-      FeaturedPosts
-    </div>
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={posts} />
+    </section>
   );
 }
 
