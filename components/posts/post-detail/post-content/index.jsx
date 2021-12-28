252de49451889f485b054741b14400ed727b84fb
Nazar <wolfter12@gmail.com>
2021-12-28 23:34:03 +0200

    add post detail

--- components/posts/post-detail/post-content/index.jsx
+++ components/posts/post-detail/post-content/index.jsx
@@ -4,16 +4,10 @@ import PostHeader from "../post-header";
 
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# This is a first post",
-};
-
-function PostContent() {
-  const { slug, title, image, content } = DUMMY_POST;
+function PostContent(props) {
+  const {
+    post: { slug, title, image, content },
+  } = props;
 
   const imagePath = `/images/posts/${slug}/${image}`;
 
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,7 +1,42 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  const { post } = props;
+
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 60,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+
+  const slugs = postFileNames.map((fileName) =>
+    fileName.replace(/\.md$/gim, "")
+  );
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug,
+      },
+    })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
