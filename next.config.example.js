7488470988ce6495dc7d1c32da36d917ed33dacf
Nazar <wolfter12@gmail.com>
2021-12-29 22:53:02 +0200

    udpate next.config.js example

--- next.config.example.js
+++ next.config.example.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
   env: {
     MONGODB_CONNECTION_URL:
-      "mongodb+srv://mongodb_name:password@cluster0.ubt42.mongodb.net",
-    MONGODB_USER_NAME: "MONGODB_USER_NAME",
-    MONGODB_PASSWORD: "MONGODB_PASSWORD",
+      "mongodb+srv://<username>:<password>@<db>",
+    MONGODB_USER_NAME: "username",
+    MONGODB_PASSWORD: "password",
   },
 };
