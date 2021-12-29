4060e51118874645ff71749e085f8055fc2c35e2
Nazar <wolfter12@gmail.com>
2021-12-29 22:37:58 +0200

    add contact form api, routes

--- components/contact/contact-form/index.jsx
+++ components/contact/contact-form/index.jsx
@@ -1,23 +1,73 @@
+import { useState } from "react";
+
 import classes from "./contact-form.module.css";
 
 function ContactForm() {
+  const [formInputValues, setFormInputValues] = useState({
+    email: "",
+    name: "",
+    message: "",
+  });
+
+  const { email, name, message } = formInputValues;
+
+  function onChangeHandler({ target: { name, value } }) {
+    setFormInputValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(formInputValues),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your email</label>
-            <input type="email" name="email" id="email" required />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={onChangeHandler}
+              required
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your name</label>
-            <input type="text" name="name" id="name" required />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={onChangeHandler}
+              required
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea name="message" id="message" rows="5"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            rows="5"
+            value={message}
+            onChange={onChangeHandler}
+            required
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
---
+++ pages/api/contact/index.js
@@ -0,0 +1,36 @@
+function validateEmail(email) {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+}
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !validateEmail(email) ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+
+    // Store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    res.status(201).json({ message: "Successfully stored message!" });
+  }
+}
+
+export default handler;
