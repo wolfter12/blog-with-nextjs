c907de4bc830bf5a1de40004f7206c5edb4f26c1
Nazar <wolfter12@gmail.com>
2021-12-29 22:01:04 +0200

    add code syntax highlighting

--- components/posts/post-detail/post-content/index.jsx
+++ components/posts/post-detail/post-content/index.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "../post-header";
 
@@ -42,6 +44,18 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+
+      const language = className.split("-").pop();
+
+      return (
+        <SyntaxHighlighter language={language} style={atomDark}>
+          {children}
+        </SyntaxHighlighter>
+      );
     },
   };
 
